5597ca2b0d4416d0fff9a67e50bf31ba7e42febe
Andre Coelho <andrecoelho@gmail.com>
2025-10-24 22:11:25 -0600

    Format

--- src/frontend/login/Login.tsx
+++ src/frontend/login/Login.tsx
@@ -21,11 +21,11 @@ export function Login() {
     });
 
     try {
-      const response = await result.json();
-
-      if (response.ok) {
+      if (result.status === 200) {
         location.reload();
       } else {
+        const response = await result.json();
+
         alertModal.open(response.error || 'Login failed');
       }
     } catch {
@@ -34,26 +34,26 @@ export function Login() {
   };
 
   return (
-    <div className="absolute inset-0 flex justify-center items-center">
-      <div className="card card-side shadow-md">
-        <div className="card-body bg-white/10 p-12 rounded-l-lg">
-          <img src={logo} className="w-96 h-64" />
+    <div className='absolute inset-0 flex justify-center items-center'>
+      <div className='card card-side shadow-md'>
+        <div className='card-body bg-white/10 p-12 rounded-l-lg'>
+          <img src={logo} className='w-96 h-64' />
         </div>
-        <div className="card-body bg-base-300 place-items-center justify-center p-12 rounded-r-lg">
-          <h1 className="card-title text-2xl mb-8">PBE Events Login</h1>
-          <form onSubmit={handleSubmit} className="grid gap-4 max-w-sm mx-auto">
-            <label className="contents">
-              <span className="self-center text-right">Email:</span>
-              <input type="email" name="email" className="input bg-base-300" required />
+        <div className='card-body bg-base-300 place-items-center justify-center p-12 rounded-r-lg'>
+          <h1 className='card-title text-2xl mb-8'>PBE Events Login</h1>
+          <form onSubmit={handleSubmit} className='grid gap-4 max-w-sm mx-auto'>
+            <label className='contents'>
+              <span className='self-center text-right'>Email:</span>
+              <input type='email' name='email' className='input bg-base-300' required />
             </label>
 
-            <label className="contents">
-              <span className="self-center text-right">Password:</span>
-              <input type="password" name="password" className="input bg-base-300" required />
+            <label className='contents'>
+              <span className='self-center text-right'>Password:</span>
+              <input type='password' name='password' className='input bg-base-300' required />
             </label>
 
-            <div className="col-span-2">
-              <button type="submit" className="btn btn-primary w-full">
+            <div className='col-span-2'>
+              <button type='submit' className='btn btn-primary w-full'>
                 Login
               </button>
             </div>
